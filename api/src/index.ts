764511a82815331017c821d4758daa59ff466042
sasaki <skyo.97.admj@gmail.com>
2021-06-27 10:19:54 +0900

    "What are you wearing today?" I get messages like

---
+++ api/src/index.ts
@@ -0,0 +1,28 @@
+// パッケージのインストール
+import { ClientConfig, Client, WebhookEvent } from '@line/bot-sdk';
+
+// アクセストークンとチャンネルシークレットをenvから読み込む
+const clientConfig: ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: process.env.CHANNEL_SECRET || '',
+};
+
+// インスタンス化
+const client: Client = new Client(clientConfig);
+
+// 実行
+exports.handler = async (event: any, context: any) => {
+  const body: any = JSON.parse(event.body);
+
+  const events: WebhookEvent[] = body.events;
+
+  events.map(
+    async (event: WebhookEvent): Promise<void> => {
+      try {
+        console.log('event: ' + JSON.stringify(event));
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  );
+};
