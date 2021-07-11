648cf7e80c9365c45764cf667b19d7e39a5336ce
sasaki <skyo.97.admj@gmail.com>
2021-07-11 14:57:48 +0900

    no message

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,10 @@
 import { ClientConfig, Client, WebhookEvent } from '@line/bot-sdk';
 import AWS from 'aws-sdk';
 
+// モジュールを読み込む
+import { ButtonMessageTemplate } from './Common/Template/ButtonMessageTemplate';
+import { ErrorMessageTemplate } from './Common/Template/ErrorMessageTemplate';
+
 // アクセストークンとチャンネルシークレットをenvから読み込む
 const clientConfig: ClientConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
@@ -10,39 +14,39 @@ const clientConfig: ClientConfig = {
 
 // インスタンス化
 const client: Client = new Client(clientConfig);
-const lambda = new AWS.Lambda();
 
 // 実行
 exports.handler = async (event: any, context: any) => {
   const body: any = JSON.parse(event.body);
+  const response: WebhookEvent = body.events[0];
 
-  const events: WebhookEvent[] = body.events;
+  try {
+    await actionButtonOrErrorMessage(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  events.map(
-    async (event: WebhookEvent): Promise<void> => {
-      try {
-        console.log('event: ' + JSON.stringify(event));
-        const params = {
-          FunctionName: 'button-error-message',
-          InvocationType: 'RequestResponse',
-          Payload: JSON.stringify({
-            client: client,
-            event: event,
-          }),
-        };
-        console.log(params);
-        const result = await lambda.invoke(params, (err, data) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(data);
-          }
-        });
-        console.log('result: ' + JSON.stringify(result));
-        console.log('result: ' + result);
-      } catch (err) {
-        console.log(err);
-      }
+// ボタンメッセージもしくはエラーメッセージを送る関数
+const actionButtonOrErrorMessage = async (event: WebhookEvent) => {
+  try {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return;
     }
-  );
+
+    const { replyToken } = event;
+    const { text } = event.message;
+
+    if (text === '今日の洋服は？') {
+      const buttonMessageTemplate = await ButtonMessageTemplate();
+      console.log('buttonMessageTemplate: ' + JSON.stringify(buttonMessageTemplate));
+      await client.replyMessage(replyToken, buttonMessageTemplate);
+    } else {
+      const errorMessageTemplate = await ErrorMessageTemplate();
+      console.log('errorMessageTemplate: ' + JSON.stringify(errorMessageTemplate));
+      await client.replyMessage(replyToken, errorMessageTemplate);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
