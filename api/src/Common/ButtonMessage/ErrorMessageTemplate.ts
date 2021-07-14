5d73ffa8224a4b2bff306aa573e298bc9bf6dd32
sasaki <skyo.97.admj@gmail.com>
2021-07-15 08:37:56 +0900

    no message

--- api/src/Common/ButtonMessage/ErrorMessageTemplate.ts
+++ api/src/Common/ButtonMessage/ErrorMessageTemplate.ts
@@ -2,11 +2,11 @@
 import { TextMessage } from '@line/bot-sdk';
 
 export const errorMessageTemplate = (): Promise<TextMessage> => {
-  const params: TextMessage = {
-    type: 'text',
-    text: 'ごめんなさい、このメッセージは対応していません。',
-  };
   return new Promise((resolve, reject) => {
+    const params: TextMessage = {
+      type: 'text',
+      text: 'ごめんなさい、このメッセージは対応していません。',
+    };
     resolve(params);
   });
 };
